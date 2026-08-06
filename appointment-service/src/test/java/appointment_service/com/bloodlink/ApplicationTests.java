1e44ee49826b99f5c678c96a268705aa9b39257b
Mendes Neto <mendesboy321@gmail.com>
2026-08-06 17:57:52 -0300

    adicionando src e pom.xml
    adicionando src e pom.xml nos respectivos modulos, onde cada um deles esta sendo gerenciado pelo maven

---
+++ appointment-service/src/test/java/appointment_service/com/bloodlink/ApplicationTests.java
@@ -0,0 +1,13 @@
+package appointment_service.com.bloodlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
